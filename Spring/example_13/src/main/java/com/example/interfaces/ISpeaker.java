91fe697cf5e3f68e3dc75478385ff3a4112bf133
mineshb <minesh.bhatiya@gmail.com>
2024-09-11 18:23:35 -0400

    Delete Spring/example_13/src/main/java/com/example directory
    Deleting local files.

--- Spring/example_13/src/main/java/com/example/interfaces/ISpeaker.java
+++
@@ -1,6 +0,0 @@
-package com.example.interfaces;
-
-public interface ISpeaker {
-
-    void makeSound();
-}
--- Spring/example_13/src/main/java/com/example/interfaces/ITyre.java
+++
@@ -1,6 +0,0 @@
-package com.example.interfaces;
-
-public interface ITyre {
-
-    void rotate();
-}
